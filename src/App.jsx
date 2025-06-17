d6b6379868974738e6b3ab314daf4a8f0cca5b6c
rjbelleza <rjbelleza19@gmail.com>
2025-06-17 22:04:30 +0800

    Added theme configuration/initial theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import './app.css'
 
 function App() {
 
   return (
     <>
-      <div className="dark">
-        <div className="bg-white dark:bg-black">
-          Hello World
-        </div>
+      <div className="text-center">
+        Hello
       </div>
     </>
   )
