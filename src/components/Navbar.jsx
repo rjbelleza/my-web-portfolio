15496b3f9c53c904826fba666fde695dfde422fd
rjbelleza <rjbelleza19@gmail.com>
2025-06-22 19:29:26 +0800

    Update: definitions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,15 @@
 import DarkModeToggle from "./DarkModeToggle.jsx";
 import { clsx } from "clsx";
-import {NavBtnFocused} from "../lib/OnFocus.js";
+import { NavBtnFocused } from "../lib/onFocus.js";
+import { navs } from "../lib/definitions.js";
 
 const Navbar = () => {
-    const navs = [
-        {name: 'Home', path: '/'},
-        {name: 'About Me', path: '/about'},
-        {name: 'Projects', path: '/projects'},
-        {name: 'Contact', path: '/contact'}
-    ];
 
     return (
         <div className="flex gap-15 items-center px-5 py-2">
             {navs.map((nav, index) => (
                 <a
-                    href="#"
+                    href={nav.path}
                     key={index}
                     className={clsx('text-[15px] font-medium',
                         {'border-b-2': NavBtnFocused(nav.path)}
---
+++ src/lib/definitions.js
@@ -0,0 +1,7 @@
+
+export const navs = [
+    {name: 'Home', path: '/'},
+    {name: 'About Me', path: '/about'},
+    {name: 'Projects', path: '/projects'},
+    {name: 'Contact', path: '/contact'}
+];
