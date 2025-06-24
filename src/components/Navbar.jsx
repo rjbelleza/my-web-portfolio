2aecf80c4e15cc2fc9342bdd82106a565d667797
rjbelleza <rjbelleza19@gmail.com>
2025-06-24 23:49:54 +0800

    Initialized Nav buttons for smaller screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import { navs } from "../lib/definitions.jsx";
 const Navbar = () => {
 
     return (
-        <div className="flex relative gap-5 bg-primary md:gap-15 items-center md:px-30 py-3 rounded-full">
-            <div className="md:h-[35px] md:w-[35px] absolute left-2 bg-primary-foreground  rounded-full">
+        <div className="flex relative gap-5 w-[200px] h-[45px] md:h-fit md:w-fit bg-primary md:gap-15 justify-center items-center md:px-30 py-3 rounded-full">
+            <div className="md:h-[35px] md:w-[35px] absolute left-2 bg-primary-foreground rounded-full">
 
             </div>
             {navs.map((nav, index) => (
@@ -19,9 +19,13 @@ const Navbar = () => {
                     >
                         {nav.name}
                     </a>
+
+                    {/* Change to icons for smaller screens */}
                     <a
                         href={nav.path}
-                        className="block md:hidden bg-white border-2 border-primary-foreground rounded-full p-2"
+                        className={clsx('flex justify-center items-center md:hidden text-secondary h-[30px] w-[30px] rounded-full transition-all ease-in-out',
+                            {'bg-secondary !text-primary': NavBtnActive(nav.path)}
+                        )}
                     >
                         {nav.icon}
                     </a>
